47ad64fbb0b279d4cee32d8090c40f2b7943d032
Gabriel Funes <gabriel.antonio.funes.s@gmail.com>
2026-08-24 15:00:02 -0300

    Commit inicial, App Base

---
+++ JobTracker.App/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace JobTracker.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "JobTracker.App" };
+        }
+    }
+}
